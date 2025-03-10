8bb11e49d0e981b1fc6dc4d4af9701b66981d681
swarupSikder <swarupfx20@gmail.com>
2025-03-10 08:51:42 +0600

    variable class

---
+++ 1000.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    //write code here
+
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    //write code here
+    printf("Hello World!\n");
+
+    return 0;
+}
